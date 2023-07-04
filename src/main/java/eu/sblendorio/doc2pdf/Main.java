db2911f4a991b3ce5dc6ab10a5c299ff337cfa0b
Francesco Sblendorio <sblendorio@MacBookProM1.local>
2023-07-04 12:34:28 +0200

    using docx4j

--- src/main/java/eu/sblendorio/doc2pdf/Main.java
+++ src/main/java/eu/sblendorio/doc2pdf/Main.java
@@ -1,7 +1,32 @@
 package eu.sblendorio.doc2pdf;
 
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.docx4j.Docx4J;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            InputStream templateInputStream = new FileInputStream("/Users/sblendorio/Documents/docsample.docx");
+            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(templateInputStream);
+            MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+
+            String outputfilepath = "/Users/sblendorio/Documents/pdfsample.pdf";
+            FileOutputStream os = new FileOutputStream(outputfilepath);
+            Docx4J.toPDF(wordMLPackage,os);
+            os.flush();
+
+            os.close();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
