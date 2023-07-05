9b6cb87eeaea3f168025df441cdcca9ee187eb39
Francesco Sblendorio <sblendorio@gmail.com>
2023-07-05 14:59:03 +0200

    update

--- src/main/java/eu/sblendorio/doc2pdf/BenchmarkHtml2Pdf.java
+++ src/main/java/eu/sblendorio/doc2pdf/BenchmarkHtml2Pdf.java
@@ -2,7 +2,7 @@ package eu.sblendorio.doc2pdf;
 
 public class BenchmarkHtml2Pdf {
     public static void main(String[] args) throws Exception {
-        final int TIMES = 3;
+        final int TIMES = 30;
         MainPd4Mlv3.main(new String[] {""+TIMES});
         MainPd4Mlv4.main(new String[] {""+TIMES});
     }
